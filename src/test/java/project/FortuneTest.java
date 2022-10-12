5fc7dc2a18defe0a4bf9ddf556c96d8f4f015203
Abhai Roop Gill <36809390+abhai28@users.noreply.github.com>
2022-10-12 12:13:29 -0400

    Created testcase for getAmount function

--- src/test/java/project/FortuneTest.java
+++ src/test/java/project/FortuneTest.java
@@ -9,4 +9,8 @@ public class FortuneTest extends TestCase{
         f = new Fortune("Captain",0);
         assertEquals("Captain",f.getName());
     }
+    public void testGetAmount(){
+        f = new Fortune("Sea Battle",2);
+        assertEquals(2,f.getAmount());
+    }
 }
