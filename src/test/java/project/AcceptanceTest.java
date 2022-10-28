bd119dd325cfd221dded7b4aed8e88c01b2ab80a
Abhai Roop Gill <36809390+abhai28@users.noreply.github.com>
2022-10-28 17:35:53 -0400

    A-Test row 48 and row 50

--- src/test/java/project/AcceptanceTest.java
+++ src/test/java/project/AcceptanceTest.java
@@ -49,5 +49,55 @@ public class AcceptanceTest extends TestCase{
         assertEquals(0,p.getScore());
     }
 
-    
+    public void testRow48(){
+        Player p = new Player(1);
+        String[] values = {"Parrot","Parrot","Parrot","Skull","Sword","Parrot","Sword","Sword"};
+        ArrayList<String> dice = new ArrayList<>(Arrays.asList(values));
+        p.setPlayerDices(dice);
+        ArrayList<Integer> diceNum = new ArrayList<>();
+        diceNum.add(4);
+        diceNum.add(6);
+        diceNum.add(7);
+        game.multiplayerReroll(p,diceNum);
+        values = new String[]{"Parrot", "Parrot", "Parrot", "Skull", "Skull", "Parrot", "Monkey", "Monkey"};
+        dice = new ArrayList<>(Arrays.asList(values));
+        p.setPlayerDices(dice);
+
+        diceNum.add(7);
+        diceNum.add(6);
+        game.multiplayerReroll(p,diceNum);
+        values = new String[]{"Parrot", "Parrot", "Parrot", "Skull", "Skull", "Parrot", "Skull", "Monkey"};
+        dice = new ArrayList<>(Arrays.asList(values));
+        p.setPlayerDices(dice);
+
+        game.calculateDiceScore(p);
+        assertEquals(0,p.getScore());
+    }
+
+    public void testRow50(){
+        Player p = new Player(1);
+        Fortune f = new Fortune("Gold",0);
+        p.setFortuneCard(f);
+        String[] values = {"Parrot","Parrot","Gold","Skull","Sword","Gold","Sword","Sword"};
+        ArrayList<String> dice = new ArrayList<>(Arrays.asList(values));
+        p.setPlayerDices(dice);
+        ArrayList<Integer> diceNum = new ArrayList<>();
+        diceNum.add(0);
+        diceNum.add(1);
+        game.multiplayerReroll(p,diceNum);
+        values = new String[]{"Gold", "Gold", "Gold", "Skull", "Sword", "Gold", "Sword", "Sword"};
+        dice = new ArrayList<>(Arrays.asList(values));
+        p.setPlayerDices(dice);
+
+        diceNum.add(4);
+        diceNum.add(6);
+        diceNum.add(7);
+        game.multiplayerReroll(p,diceNum);
+        values = new String[]{"Gold", "Gold", "Gold", "Skull", "Gold", "Gold", "Gold", "Gold"};
+        dice = new ArrayList<>(Arrays.asList(values));
+        p.setPlayerDices(dice);
+
+        game.calculateDiceScore(p);
+        assertEquals(4800,p.getScore());
+    }
 }
