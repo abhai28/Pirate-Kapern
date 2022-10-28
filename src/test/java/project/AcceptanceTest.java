dcf4188c092837f6c42f53bbf80fc6fee1fdcc8f
Abhai Roop Gill <36809390+abhai28@users.noreply.github.com>
2022-10-28 13:44:42 -0400

    A-Test Row 45

--- src/test/java/project/AcceptanceTest.java
+++ src/test/java/project/AcceptanceTest.java
@@ -1,11 +1,20 @@
 package project;
 import junit.framework.TestCase;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class AcceptanceTest extends TestCase{
     Game game;
-    Player p;
     public AcceptanceTest (){
         game = new Game();
-        p = new Player(1);
+    }
+    public void testRow45(){
+        Player p = new Player(1);
+        String[] values = {"Skull","Sword","Sword","Skull","Sword","Skull","Sword","Sword"};
+        ArrayList<String> dice = new ArrayList<>(Arrays.asList(values));
+        p.setPlayerDices(dice);
+        game.calculateDiceScore(p);
+        assertEquals(0,p.getScore());
     }
 }
