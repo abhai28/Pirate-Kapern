37ec36dc98dce2fab0ada70654bf3c4dcbec45ac
Abhai Roop Gill <36809390+abhai28@users.noreply.github.com>
2022-11-09 14:51:56 -0500

    Refactor commit

--- src/main/java/project/Player.java
+++ src/main/java/project/Player.java
@@ -7,18 +7,29 @@ public class Player {
     private int playerID;
     private ArrayList<String> dice;
     private int score;
-
+    private ArrayList<String> treasureChest;
     public Player(){
         fortuneCard = new Fortune();
         playerID = 1;
         dice = new ArrayList<>();
+        treasureChest = new ArrayList<>();
         score = 0;
     }
     public Player(int id){
         fortuneCard = new Fortune();
         playerID = id;
         dice = new ArrayList<>();
+        treasureChest = new ArrayList<>();
         score = 0;
+    }
+    public void addTreasure(int i){
+        treasureChest.add(dice.remove(i));
+    }
+    public void removeTreasure(int i){
+        dice.add(treasureChest.remove(i));
+    }
+    public ArrayList<String> getTreasureChest(){
+        return treasureChest;
     }
     public void setFortuneCard(Fortune f){
         fortuneCard = f;
