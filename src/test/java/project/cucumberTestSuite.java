aef51c1d7c994cee2dbe1d21ca996ec4ed5bf2ab
Abhai Roop Gill <36809390+abhai28@users.noreply.github.com>
2022-11-12 17:32:01 -0500

    Refactor commit

--- src/test/java/project/cucumberTestSuite.java
+++ src/test/java/project/cucumberTestSuite.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = "message:target/cucumber-report.ndjson", features = "src/test/java/project")
+@CucumberOptions(plugin = "message:target/cucumber-report.ndjson", features = "src/test/java/resources")
 public class cucumberTestSuite {
 
 }
